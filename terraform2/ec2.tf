796b054474fbd6e170f24fdf9afebc183e6cc624
wanedri <wanedri95@gmail.com>
2026-07-30 22:22:24 +0800

    Add ec2.tf extra data block

--- terraform2/ec2.tf
+++ terraform2/ec2.tf
@@ -11,3 +11,16 @@ data "aws_ami" "my_ami" {
 data "aws_iam_instance_profile" "my_ssm_profile" {
   name = "EC2-SSM-Role"
 }
+
+# < bersambung
+resource "aws_instance" "my_server_1" {
+  ami                    = data.aws_ami.my_ami.id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.my_subnet.id
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+  iam_instance_profile   = data.aws_iam_instance_profile.my_ssm_profile.name
+
+  tags = {
+    Name = "tf-server-1"
+  }
+}
