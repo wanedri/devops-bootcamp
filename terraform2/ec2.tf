8240cf8a96339758be34703b401a3903f14b5f68
wanedri <wanedri95@gmail.com>
2026-07-30 22:05:41 +0800

    Add ec2.tf with data block

---
+++ terraform2/ec2.tf
@@ -0,0 +1,13 @@
+data "aws_ami" "my_ami" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+}
+
+data "aws_iam_instance_profile" "my_ssm_profile" {
+  name = "EC2-SSM-Role"
+}
