151afc9d9d991d99788f4380bdaa758e1290843b
wanedri <wanedri95@gmail.com>
2026-08-18 11:27:27 +0800

    Ansible 2: Template+handler

---
+++ ansible2/ec2.tf
@@ -0,0 +1,37 @@
+data "aws_ami" "my_ami" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+}
+
+module "node1" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 6.0"
+  name                   = "node1"
+  ami                    = data.aws_ami.my_ami.id
+  instance_type          = "t3.micro"
+  subnet_id              = module.my_vpc.public_subnets[0]
+  create_security_group  = false
+  vpc_security_group_ids = [module.my_sg.id]
+  key_name               = "wan-adri-key"
+  tags                   = { Name = "node1" }
+  root_block_device = { size = 16 }
+}
+
+module "node2" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 6.0"
+  name                   = "node2"
+  ami                    = data.aws_ami.my_ami.id
+  instance_type          = "t3.micro"
+  subnet_id              = module.my_vpc.public_subnets[0]
+  create_security_group  = false
+  vpc_security_group_ids = [module.my_sg.id]
+  key_name               = "wan-adri-key"
+  tags                   = { Name = "node2" }
+  root_block_device = { size = 16 }
+}
