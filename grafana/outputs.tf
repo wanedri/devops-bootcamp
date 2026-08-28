553637302c0c12832f64a88a16ebc840da31cfa2
wanedri <wanedri95@gmail.com>
2026-08-28 10:37:32 +0800

    Grafana

---
+++ grafana/outputs.tf
@@ -0,0 +1,5 @@
+output "node1_ip" { value = module.node1.public_ip }
+output "node2_ip" { value = module.node2.public_ip }
+
+output "node1_private_ip" { value = module.node1.private_ip }
+output "node2_private_ip" { value = module.node2.private_ip }
